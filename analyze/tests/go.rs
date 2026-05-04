2f1e54a17e93791cea30ac142c0e9f26fdc4959c
Felix Drodofsky <drodofsky@protonmail.com>
2026-05-04 10:27:08 +0200

    add test which can be used to create a flamegraph

--- analyze/tests/go.rs
+++ analyze/tests/go.rs
@@ -14,3 +14,14 @@ fn m1() {
         vec![u8::MAX, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
     );
 }
+#[ignore = "reason"]
+#[test]
+fn profile_match() {
+    let map = load_kanji_map();
+    let reference = analyzed(&map, '語');
+    let user = load_test_file("語_m1");
+
+    for _ in 0..500 {
+        let _ = match_node(&reference, &user);
+    }
+}
