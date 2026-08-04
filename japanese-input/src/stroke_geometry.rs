b7a54d61353c5bfb51dab3b3e2a66910210716be
Felix Drodofsky <drodofsky@protonmail.com>
2026-08-04 14:00:28 +0200

    add arc_len to StrokeGeometry

--- japanese-input/src/stroke_geometry.rs
+++ japanese-input/src/stroke_geometry.rs
@@ -1,12 +1,15 @@
 use kurbo::{Point, Rect};
 
-use crate::{bbox::BBox as _, centroid::Centroid as _, stroke_point::StrokePoint};
+use crate::{
+    arc_len::ArcLen as _, bbox::BBox as _, centroid::Centroid as _, stroke_point::StrokePoint,
+};
 
 #[non_exhaustive]
 #[derive(Debug, Clone, Copy)]
 pub struct StrokeGeometry {
     pub bbox: Option<Rect>,
     pub centroid: Option<Point>,
+    pub arc_len: f64,
 }
 
 impl StrokeGeometry {
@@ -15,6 +18,11 @@ impl StrokeGeometry {
     pub fn from_stroke(stroke: &[StrokePoint]) -> Self {
         let bbox = stroke.bbox();
         let centroid = stroke.centroid();
-        Self { bbox, centroid }
+        let arc_len = stroke.arc_len();
+        Self {
+            bbox,
+            centroid,
+            arc_len,
+        }
     }
 }
